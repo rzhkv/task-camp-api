e2f8cf81003f76b03a14b8bc087586d5e45dd087
Denis <rdrozhkov@gmail.com>
2023-09-25 13:57:29 +0300

    Добавить таблицу space

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,20 +1,27 @@
 import express from 'express';
 import validate from '../app/Middleware/validate.js';
 
-import { createUserDataSchema, loginUserDataSchema } from '../app/Validations/user.validation.js';
-import { createUserController, loginUserController } from '../app/Controllers/user.controller.js';
+import { signupUserDataSchema, signinUserDataSchema, resetUserDataSchema } from '../app/Validations/user.validation.js';
 
-import 'express-router-group';
+import {
+  createUserController,
+  loginUserController,
+  resetPasswordUserController,
+} from '../app/Controllers/user.controller.js';
+
+import { createSpaceController, addUserIntoSpaceController } from '../app/Controllers/space.controller.js';
+
 const router = express.Router();
 
-router.group('/user', (router) => {
-  router.post('/register', validate(createUserDataSchema), createUserController);
-  router.post('/login', validate(loginUserDataSchema), loginUserController);
-});
+router.post('/signup', validate(signupUserDataSchema), createUserController);
+router.post('/signin', validate(signinUserDataSchema), loginUserController);
+router.post('/reset', validate(resetUserDataSchema), resetPasswordUserController);
 
-router.group('/project', (router) => {
-  // router.post('/add', validate(addProjectDataSchema), createUserController);
-  // router.delete('/login', validate(loginUserDataSchema), loginUserController);
-});
+router.post('/space/create', createSpaceController);
+router.post('/space/invite', addUserIntoSpaceController);
+
+router.post('/space/projects')
+router.зге('/space/projects')
+router.delete('/space/projects')
 
 export default router;
